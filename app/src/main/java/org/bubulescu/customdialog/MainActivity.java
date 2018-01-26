938722e5235a0736a29a8359612efd6204c40d62
Marijo <marijo.novosel@predavaci.algebra.hr>
2018-01-26 12:11:47 +0100

    init

---
+++ app/src/main/java/org/bubulescu/customdialog/MainActivity.java
@@ -0,0 +1,34 @@
+package org.bubulescu.customdialog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnShow;
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShow = findViewById(R.id.btnShow);
+        tvResult = findViewById(R.id.tvResult);
+
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomDialog customDialog = new CustomDialog();
+                customDialog.show(getSupportFragmentManager(), "cd");
+            }
+        });
+    }
+
+    public void setTextToTextView (String text) {
+        tvResult.setText(text);
+    }
+}
